0b570eba7c00cc7565d5813ec9152311f0f277ff
sunita <sunitamaurya.std@gmail.com>
2023-03-28 11:39:04 +0530

    change code

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,51 +1,57 @@
 import React from "react";
-import './Accordion.css'
 import { IoIosArrowDown } from "react-icons/io";
 const Accordion = () => {
-  const [check, setCheck] = React.useState(false);
   return (
     <div className=" py-32 ">
       <div className=" text-center ">
         <h3>FLUTTERFLIES</h3>
-        <h1 className="w-full">CLARITY OF FUTURE</h1>
+        <h1 className="w-full">ASKED QUESTIONS</h1>
       </div>
-      <div className="tabs-box">
+      <div className="tabs-box  md:text-auto text-sm ">
         <div className="acord ">
           <input type="checkbox" name="mytabs" id="tb-one" />
           <div className="relative flex items-center pl-5 summary">
             <label htmlFor="tb-one" className="  w-full bg-one md:leading-10">
               Lorem ipsum is placeholder text commonly used in the graphic
             </label>
-            <IoIosArrowDown className="accordArrow" />
+            <div className="arrow-border">
+              <IoIosArrowDown className="accordArrow" />
+            </div>
           </div>
           <div className="content">
-            <span className="square"></span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip commodo consequat. Duis aute irure dolor in reprehenderit in
-            voluptate velit esse cillum dolore eu fugi pariatur. Excepteur sint
-            occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            <span className="pt-3 pr-5 block md:text-auto text-sm">
+              wwew Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+              do eiusmod tempor incididunt ut labore et magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugi pariatur. Excepteur
+              sint occaecat cupidatat non proident, sunt in culpa qui officia
+              deserunt
+            </span>
           </div>
         </div>
 
         <div className="acord">
           <input type="checkbox" name="mytabs" id="tb-two" />
           <div className="relative flex pl-5 summary">
-            <label htmlFor="tb-two" className="w-full bg-two md:leading-10">
+            <label htmlFor="tb-two" className="w-full  bg-two md:leading-10">
               Lorem ipsum is placeholder text commonly used in the graphic
             </label>
-            <IoIosArrowDown className="accordArrow" />
+            <div className="arrow-border">
+              <IoIosArrowDown className="accordArrow" />
+            </div>
           </div>
 
           <div className="content">
-            <span className="square"></span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip commodo consequat. Duis aute irure dolor in reprehenderit in
-            voluptate velit esse cillum dolore eu fugi pariatur. Excepteur sint
-            occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            <span className="pt-3 pr-5 block md:text-auto text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugi pariatur. Excepteur
+              sint occaecat cupidatat non proident, sunt in culpa qui officia
+              deserunt
+            </span>
           </div>
         </div>
 
@@ -59,17 +65,21 @@ const Accordion = () => {
             >
               Lorem ipsum is placeholder text commonly used in the graphic
             </label>
-            <IoIosArrowDown className=" accordArrow " />
+            <div className="arrow-border">
+              <IoIosArrowDown className=" accordArrow " />
+            </div>
           </div>
 
           <div className="content">
-            <span className="square"></span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip commodo consequat. Duis aute irure dolor in reprehenderit in
-            voluptate velit esse cillum dolore eu fugi pariatur. Excepteur sint
-            occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            <span className="pt-3 pr-5 block md:text-auto text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugi pariatur. Excepteur
+              sint occaecat cupidatat non proident, sunt in culpa qui officia
+              deserunt
+            </span>
           </div>
         </div>
         <div className="acord">
@@ -82,17 +92,21 @@ const Accordion = () => {
             >
               Lorem ipsum is placeholder text commonly used in the graphic
             </label>
-            <IoIosArrowDown className=" accordArrow " />
+            <div className="arrow-border">
+              <IoIosArrowDown className=" accordArrow " />
+            </div>
           </div>
 
           <div className="content">
-            <span className="square"></span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip commodo consequat. Duis aute irure dolor in reprehenderit in
-            voluptate velit esse cillum dolore eu fugi pariatur. Excepteur sint
-            occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            <span className="pt-3 pr-5 block md:text-auto text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugi pariatur. Excepteur
+              sint occaecat cupidatat non proident, sunt in culpa qui officia
+              deserunt
+            </span>
           </div>
         </div>
       </div>
